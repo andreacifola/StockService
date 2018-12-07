3e166fa62bcf0ac08982d8c5e7503becf45899f1
Andrea Cifola <cifola.andrea@gmail.com>
2018-12-07 09:51:27 +0100

    Refactor Main class

--- src/main/java/com/example/demo/StockServiceMain.java
+++ src/main/java/com/example/demo/StockServiceMain.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class StockServiceApplication {
+public class StockServiceMain {
 
 	public static void main(String[] args) {
-		SpringApplication.run(StockServiceApplication.class, args);
+		SpringApplication.run(StockServiceMain.class, args);
 	}
 }
